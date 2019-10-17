787d21b492eb37a9e8bd2b90f5ea9b53780b4ee5
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-10-17 16:53:49 -0700

    created delete function

--- src/ApiContext.js
+++ src/ApiContext.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 const ApiContext = React.createContext({
     notes: [],
-    folders: []
+    folders: [],
+    deleteNote: () => {}
 })
 
 export default ApiContext
--- src/App/App.js
+++ src/App/App.js
@@ -18,25 +18,31 @@ class App extends Component {
 
     componentDidMount() {
         // fake date loading from API call
-        setTimeout(() => this.setState(dummyStore), 600);
-        // Promise.all([
-        //     fetch(`${config.API_ENDPOINT}/notes`),
-        //     fetch(`${config.API_ENDPOINT}/folders`)
-        // ])
-        //     .then(([notesRes, foldersRes]) => {
-        //         if (!notesRes.ok)
-        //             return notesRes.json().then(e => Promise.reject(e));
-        //         if (!foldersRes.ok)
-        //             return foldersRes.json().then(e => Promise.reject(e));
+        // setTimeout(() => this.setState(dummyStore), 600);
+        Promise.all([
+            fetch(`${config.API_ENDPOINT}/notes`),
+            fetch(`${config.API_ENDPOINT}/folders`)
+        ])
+            .then(([notesRes, foldersRes]) => {
+                if (!notesRes.ok)
+                    return notesRes.json().then(e => Promise.reject(e));
+                if (!foldersRes.ok)
+                    return foldersRes.json().then(e => Promise.reject(e));
 
-        //         return Promise.all([notesRes.json(), foldersRes.json()]);
-        //     })
-        //     .then(([notes, folders]) => {
-        //         this.setState({notes, folders});
-        //     })
-        //     .catch(error => {
-        //         console.error({error});
-        //     });
+                return Promise.all([notesRes.json(), foldersRes.json()]);
+            })
+            .then(([notes, folders]) => {
+                this.setState({notes, folders});
+            })
+            .catch(error => {
+                console.error({error});
+            })
+    }
+
+    handleDeleteNote = noteId => {
+        this.setState({
+            notes: this.state.notes.filter(note => note.id !== noteId)
+        })
     }
 
     renderNavRoutes() {
@@ -76,7 +82,8 @@ class App extends Component {
     render() {
         const value = {
             notes: this.state.notes,
-            folders: this.state.folders
+            folders: this.state.folders,
+            deleteNote: this.handleDeleteNote
         }
         return (
             <ApiContext.Provider value={value}>
